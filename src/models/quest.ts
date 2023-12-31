c6483092ec3730e664366b214d6cd76380067a0d
Alexander Kern <alexander.t.kern@gmail.com>
2023-12-31 14:07:09 -0800

    Quests and Frequency

--- src/models/quest.ts
+++ src/models/quest.ts
@@ -1,13 +1,40 @@
+export enum Frequency {
+    Once = "once",
+    Daily = "daily",
+    Weekly = "weekly",
+    Monthly = "monthly",
+}
+
+
 export class Quest {
+    public progress: { [key: string]: number | boolean};
+    public completed: boolean;
+    
     constructor(
-      public id: string,
-      public objective: string,
-      public questText: string,
-      public reward: string,
-      // Add other properties as needed
-    ) {}
+        public id: number,
+        public title: string,
+        public questText: string,
+        public requirements: { [key: string]: number | boolean },
+        public reward: Array<any>,
+        public repeatable: boolean = false,
+        public frequency: Frequency = Frequency.Once,
+    ) {
+        this.progress = {};
+        this.completed = false;
+        for (let key in requirements) {
+            typeof(this.requirements[key]) === 'number' ? this.progress[key] = 0 : this.progress[key] = false;  
+        }
+    }
   
-    complete() {
-      // Add code to complete the quest
+    
+
+    completeQuest(): boolean {
+        for (let key in this.progress) {
+            if (this.progress[key] !== this.requirements[key]) {
+                    return false;
+            }
+        }
+        this.completed = true;
+        return true;
     }
   }

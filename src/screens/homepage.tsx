e597d4d60dabf0bc48a2a822df96b5566ae35c77
Alexander Kern <alexander.t.kern@gmail.com>
2024-01-01 10:10:58 -0800

    Import Native Navigation

---
+++ src/screens/homepage.tsx
@@ -0,0 +1,13 @@
+// HomePage.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomePage = () => {
+  return (
+    <View>
+      <Text>Welcome to the Home Page!</Text>
+    </View>
+  );
+};
+
+export default HomePage;
